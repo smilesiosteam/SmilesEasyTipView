5985e6e7bf27d2b76b390b7af50e2b9c2e7e9fa3
Habib Rehman <haburrehman@eand.com>
2023-10-03 14:06:31 +0500

    -updated package dependencies

--- Package.swift
+++ Package.swift
@@ -17,8 +17,9 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/smilesiosteam/SmilesUtilities.git", branch: "main"),
-        .package(url: "https://github.com/smilesiosteam/SmilesLanguageManager.git", branch: "main")
+        .package(url: "https://github.com/smilesiosteam/SmilesUtilities.git", branch: "shared_Models"),
+        .package(url: "https://github.com/smilesiosteam/SmilesLanguageManager.git", branch: "main"),
+        .package(url: "https://github.com/smilesiosteam/SmilesSharedModels.git", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -27,7 +28,8 @@ let package = Package(
             name: "SmilesEasyTipView",
             dependencies: [
                 .product(name: "SmilesUtilities", package: "SmilesUtilities"),
-                .product(name: "SmilesLanguageManager", package: "SmilesLanguageManager")
+                .product(name: "SmilesLanguageManager", package: "SmilesLanguageManager"),
+                .product(name: "SmilesSharedModels", package: "SmilesSharedModels")
             ]),
         .testTarget(
             name: "SmilesEasyTipViewTests",
